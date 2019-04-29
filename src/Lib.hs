14859c396b6a514b26d75520187220e93325e4b9
Hoai-Nam NGUYEN <Hoai-nam.NGUYEN@ingenico.com>
2019-04-29 17:34:31 +0200

    finish getChildren function

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,26 +1,63 @@
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc
+  ) where
 
+data GameTree a
+  = TerminatedNode a
+  | PlayingNode a
+                (Maybe [GameTree a])
+  deriving (Show)
 
-data GameTree a = TerminatedNode a | PlayingNode a [GameTree a] deriving (Show)
-
-data GameTreeCrumb a = GameTreeCrumb a [GameTree a] [GameTree a] deriving (Show)
+data GameTreeCrumb a =
+  GameTreeCrumb a
+                [GameTree a]
+                [GameTree a]
+  deriving (Show)
 
 type GameTreeZipper a = (GameTree a, [GameTreeCrumb a])
 
 gameTreeUp :: GameTreeZipper a -> Maybe (GameTreeZipper a)
 gameTreeUp (currentGameTree, []) = Nothing
-gameTreeUp (currentGameTree, GameTreeCrumb d ls rs:bs) = Just (PlayingNode d (ls ++ [currentGameTree] ++ rs), bs)
+gameTreeUp (currentGameTree, GameTreeCrumb d ls rs:bs) =
+  Just (PlayingNode d (Just (ls ++ [currentGameTree] ++ rs)), bs)
 
--- gameTreeFocus :: GameTreeZipper a -> Maybe (GameTreeZipper a)
--- gameTreeFocus (TerminatedNode _, _) = Nothing
--- gameTreeFocus (PlayingNode d childNodes, _) = Nothing
+getElementWithLeftAndRight :: [a] -> [(a, [a], [a])]
+getElementWithLeftAndRight list =
+  foldr
+    (\currentElem currentResult ->
+       case currentResult of
+         [] -> [(currentElem, init list, [])]
+         (last, lastLeft, lastRight):r ->
+           (currentElem, init lastLeft, last : lastRight) : currentResult)
+    []
+    list
 
+data Cell
+  = NotPlayed
+  | Self
+  | Opponent
 
--- getChildren :: GameTreeZipper a -> [GameTreeZipper a]
--- getChildren (TerminatedNode _, _) = []
--- getChildren (PlayingNode d children, treeCrumbs) = children
+data NextPlayer = SelfPlayer | OpponentPlayer
+
+data Board = Board NextPlayer [[Cell]]
+
+data VisitedBoard = VisitedBoard
+  { q     :: Int
+  , n     :: Int
+  , board :: Board
+  }
+
+data GameNode
+  = Unvisited Board
+  | Visited VisitedBoard
+
+getChildren :: GameTreeZipper a -> [GameTreeZipper a]
+getChildren (TerminatedNode _, _) = []
+getChildren (PlayingNode d (Just children), treeCrumbs) =
+  let withLeftAndRight = getElementWithLeftAndRight children
+      convertToZipper (elem, left, right) =
+        (elem, GameTreeCrumb d left right : treeCrumbs)
+   in map convertToZipper withLeftAndRight
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,10 @@
-import Test.HUnit
+import           Test.HUnit
 
 test1 = TestCase (assertEqual "shouldBeGood" 1 1)
+
 tests = TestList [TestLabel "test1" test1]
-
-
 
 main :: IO ()
 main = do
-        counts <- runTestTT tests
-        putStrLn $ show counts
+  counts <- runTestTT tests
+  putStrLn $ show counts
