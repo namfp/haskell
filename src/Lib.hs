379358c33966a1c42dd42a9c556b88913f90fdb1
Hoai-Nam NGUYEN <Hoai-nam.NGUYEN@ingenico.com>
2019-04-30 16:56:22 +0200

    nextMoves

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,9 +2,8 @@ module Lib
   ( someFunc
   ) where
 
+import           Data.Array
 import           Data.List
-import Data.Array.IArray
-
 
 data GameTree a
   = TerminatedNode a
@@ -48,44 +47,96 @@ data Cell
   = NotPlayed
   | Self
   | Opponent
+  deriving (Eq)
 
 data Player
   = SelfPlayer
   | OpponentPlayer
-  deriving (Eq)
+  deriving (Eq, Show)
 
 type NextPlayer = Player
 
-data Board =
-  Board NextPlayer
-        [[Cell]]
+type Board = (Array (Int, Int) Cell)
+
+data GameState =
+  GameState NextPlayer
+            Board
 
 data VisitedBoard = VisitedBoard
   { q     :: Int
   , n     :: Int
-  , board :: Board
+  , board :: GameState
   }
 
 data GameNode
-  = Unvisited Board
+  = Unvisited GameState
   | Visited VisitedBoard
 
--- computeWinner :: Board -> Maybe Player
--- computeWinner (Board nextPlayer board) =
---   let currentPlayer =
---         case nextPlayer of
---           SelfPlayer     -> OpponentPlayer
---           OpponentPlayer -> SelfPlayer
---       playedCell player cell =
---         case cell of
---           NotPlayed -> False
---           Self      -> currentPlayer == SelfPlayer
---           Opponent  -> currentPlayer == OpponentPlayer
---       isLineWinner player line = all (playedCell player) line
---       isLinesWinner player = any (isLineWinner player) board
---       transposedBoard = transpose board
---       isColumnsWinner player = isLinesWinner player transposedBoard
-   in Nothing
+computeWinner :: Board -> Maybe Player
+computeWinner board =
+  let winPositions :: [[(Int, Int)]]
+      winPositions =
+        [ [(1, 1), (2, 1), (3, 1)]
+        , [(1, 2), (2, 2), (3, 2)]
+        , [(1, 3), (2, 3), (3, 3)]
+        , [(1, 1), (1, 2), (1, 3)]
+        , [(2, 1), (2, 2), (2, 3)]
+        , [(3, 1), (3, 2), (3, 3)]
+        , [(1, 1), (2, 2), (3, 3)]
+        , [(1, 3), (2, 2), (3, 1)]
+        ]
+      matchCell :: Cell -> (Int, Int) -> Bool
+      matchCell player (i, j) = (board ! (i, j)) == player
+      isWinPosition :: Cell -> [(Int, Int)] -> Bool
+      isWinPosition player position = all (matchCell player) position
+      isWin :: Cell -> Bool
+      isWin player = any (isWinPosition player) winPositions
+   in if (isWin Self)
+        then Just SelfPlayer
+        else if (isWin Opponent)
+               then Just OpponentPlayer
+               else Nothing
+
+b :: Board
+b =
+  listArray
+    ((1, 1), (3, 3))
+    [ Self
+    , Self
+    , Self
+    , Opponent
+    , NotPlayed
+    , NotPlayed
+    , Opponent
+    , Opponent
+    , NotPlayed
+    ]
+
+-- simulate a node until the end
+simulate :: GameTreeZipper GameNode -> GameTreeZipper GameNode
+simulate (TerminatedNode t, b) = (TerminatedNode t, b)
+
+-- simulate (PlayingNode )
+computeNextPlayer :: Player -> Player
+computeNextPlayer OpponentPlayer = SelfPlayer
+computeNextPlayer SelfPlayer     = OpponentPlayer
+
+nextMoves :: GameState -> [GameState]
+nextMoves (GameState nextPlayer board) =
+  let nextCell =
+        case nextPlayer of
+          SelfPlayer     -> Self
+          OpponentPlayer -> Opponent
+      playNextMove (i, j) board = board // [((i, j), nextCell)]
+      possibilities =
+        [ nextGameState
+        | i <- [1 .. 3]
+        , j <- [1 .. 3]
+        , (board ! (i, j)) == NotPlayed
+        , let nextBoard = playNextMove (i, j) board
+        , let nextGameState = GameState (computeNextPlayer nextPlayer) nextBoard
+        ]
+   in possibilities
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
